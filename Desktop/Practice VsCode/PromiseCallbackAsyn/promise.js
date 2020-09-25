d1dc4e2bd0fccd4fca07ddad21c67aa51e78502d
Mary <maryolorunfemi6@gmail.com>
2020-09-25 09:23:26 +0100

    asyn await practice

--- Desktop/Practice VsCode/PromiseCallbackAsyn/promise.js
+++ Desktop/Practice VsCode/PromiseCallbackAsyn/promise.js
@@ -7,39 +7,60 @@ const posts = [
 
 
 
+function  getPosts(){
+  setTimeout(()=>{
+    let output = ''
+    posts.forEach((post) =>{
+      output += `<h1>${post.title}<h1>`
+    });
+    document.body.innerHTML = output
+  }, 2000);
+}
 
-// function  getPosts(){
-//   setTimeout(()=>{
-//     let output = ''
-//     posts.forEach((post, index) =>{
-//       output += `<h1>${post.title}<h1>`
-//     });
-//     document.body.innerHTML = output
-//   }, 2000);
-// }
-
-// function createPost(post){
-//   return new Promise((resolve, reject)=>{
-//     setTimeout(() =>{
-//       posts.push(post);
-//   }, 1000);
-//   const error = false;
-//   if(!error){
-//     resolve();
-// }else{
-//   reject('something went wrong');
-// }
+function createPost(post){
+  return new Promise((resolve, reject)=>{
+    setTimeout(() =>{
+      posts.push(post);
+  }, 1000);
+  const error = false;
+  if(!error){
+    resolve();
+}else{
+  reject('something went wrong');
+}
    
-//   }, 2000);
+  }, 2000);
+
+}
+
+// Async Await
+
+async function init(){
+  await createPost({title: '2ndcodes', body: 'Geek Oiza2'})
+  getPosts();
+}
+
+init();
+
+// async function fetchUsers(){
+
+//   const data = fetch('https://jsonplaceholder.typicode.com/users');
+  
 
 // }
+
+
+
+
+
+
 // createPost({title: 'codes', body: 'Geek Oiza3'})
 // .then(getPosts)
 // .catch(err => console.log(err));
 
-const promise1 = Promise.resolve('Geek Oiza');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => 
-setTimeout(resolve,'Good Bye', 2000));
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
-Promise.all([promise1, promise2, promise3, promise4]).then(values =>console.log(values));
+// const promise1 = Promise.resolve('Geek Oiza');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => 
+// setTimeout(resolve,'Good Bye', 2000));
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+// Promise.all([promise1, promise2, promise3, promise4]).then(values =>console.log(values));
